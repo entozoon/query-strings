fd974165bcd0aff0fe2fa3eff61cf1becfade176
Michael Cook <michael.cook@thetechdept.com>
2019-12-13 15:10:44 +0000

    Okay, we've got our params

--- app.js
+++ app.js
@@ -1,1 +1,6 @@
-console.log(window.location);
+var urlParams = new URLSearchParams(window.location.search);
+var params = [];
+urlParams.forEach(function (value, key) {
+    params.push({ key: key, value: value });
+});
+console.log(params);
--- app.ts
+++ app.ts
@@ -1,1 +1,8 @@
-console.log(window.location);
+const urlParams = new URLSearchParams(window.location.search);
+
+const params = [];
+urlParams.forEach((value, key) => {
+  params.push({ key, value });
+});
+
+console.log(params);
